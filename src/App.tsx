aa49b97028b96591e084a9501e92df80ff4af447
violettomania <danielpolya@protonmail.com>
2023-11-05 16:37:33 +0100

    add images and initial code

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,47 @@
 export default function App() {
   return (
     <main>
-      <div>
-        <p>it works!</p>
-      </div>
+      <section className='menu'>
+        <div className='title'>
+          <h2>our menu</h2>
+          <div className='title-underline'></div>
+        </div>
+        <div className='btn-container'>
+          <button type='button' className='btn'>
+            all
+          </button>
+          <button type='button' className='btn'>
+            breakfast
+          </button>
+          <button type='button' className='btn'>
+            lunch
+          </button>
+          <button type='button' className='btn'>
+            shakes
+          </button>
+        </div>
+        <div className='section-center'>
+          <article className='menu-item'>
+            <img
+              src='../images/item-1.jpeg'
+              alt='buttermilk pancakes'
+              className='img'
+            />
+            <div className='item-info'>
+              <header>
+                <h5>buttermilk pancakes</h5>
+                <span className='item-price'>$15.99</span>
+              </header>
+              <p className='item-text'>
+                I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock
+                freegan copper mug whatever cold-pressed I'm baby woke mlkshk
+                wolf bitters live-edge blue bottle, hammock freegan copper mug
+                whatever cold-pressed{' '}
+              </p>
+            </div>
+          </article>
+        </div>
+      </section>
     </main>
   );
 }
